07b1e36abfedc30eea8b87bad3766df5282a1ce5
Warsel <walot04@gmail.com>
2018-11-19 22:03:34 +0300

    Release-0.1. Fixed getting version code and changed some colors. Migrate to AndroidX

--- app/src/main/java/walot/softwaredesign/MainActivity.kt
+++ app/src/main/java/walot/softwaredesign/MainActivity.kt
@@ -1,14 +1,15 @@
 package walot.softwaredesign
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
-import android.support.v7.app.AlertDialog
-import android.support.v7.app.AppCompatActivity
 import android.telephony.TelephonyManager
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -25,7 +26,24 @@ class MainActivity : AppCompatActivity() {
 
         version_info_tv.text = getString(R.string.version_info, projectVersion)
 
-        requestPermissions(permissions, REQUEST_CODE)
+        get_imei_btn.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage(getString(R.string.about_permission))
+                    .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                        requestPermissions(permissions, REQUEST_CODE)
+                    }
+                    .setNegativeButton(getString(R.string.no)) { _, _ -> }
+                    .create()
+                    .show()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStart() {
+        checkPermission()
+        super.onStart()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -33,19 +51,43 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             REQUEST_CODE -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-                    val imei = telephonyManager.imei
-                    imei_info_tv.text = getString(R.string.imei_info, imei)
+                    imei_info_tv.text = getString(R.string.imei_info, getImei())
                 }
                 else {
-                    val builder = AlertDialog.Builder(this)
-                    builder.setMessage(getString(R.string.about_permission))
-                        .setPositiveButton(getString(R.string.clear)) { _, _ -> }
-                        .create()
-                        .show()
+                    if (!shouldShowRequestPermissionRationale(permissions[0])) {
+                        val builder = AlertDialog.Builder(this)
+                        builder.setMessage(getString(R.string.set_permission_yourself))
+                            .setPositiveButton(getString(R.string.clear)) { _, _ -> }
+                            .create()
+                            .show()
+                    }
+                    else {
+                        val builder = AlertDialog.Builder(this)
+                        builder.setMessage(getString(R.string.about_permission_decline))
+                            .setPositiveButton(getString(R.string.sure)) { _, _ -> }
+                            .setNegativeButton(getString(R.string.not_sure)) { _, _ ->
+                                requestPermissions(permissions, requestCode)
+                            }
+                            .create()
+                            .show()
+                    }
                 }
                 return
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun checkPermission() {
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            imei_info_tv.text = getString(R.string.imei_info, getImei())
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("MissingPermission")
+    fun getImei() : String {
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        return telephonyManager.imei
     }
 }
